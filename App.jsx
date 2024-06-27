cea87624f38cab5c5674a05def64c71c8f314b15
Hare Preya <110776008+harepreya@users.noreply.github.com>
2024-06-27 15:00:15 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Todo from "./components/Todo";
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-900">
+      <Todo />
+    </div>
+  );
+};
+
+export default App;
+
